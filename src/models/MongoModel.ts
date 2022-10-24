ccb6d6059ffef8fe839ec0a21f128957676d49b3
ed-cavalcanti <edney2809@gmail.com>
2022-10-24 16:54:18 -0300

    [feat] add MongoModel

---
+++ src/models/MongoModel.ts
@@ -0,0 +1,23 @@
+import { Model } from 'mongoose';
+import { IModel } from '../interfaces/IModel';
+
+abstract class MongoModel<T> implements IModel<T> {
+  protected _model: Model<T>;
+
+  constructor(model: Model<T>) {
+    this._model = model;
+  }
+
+  create = async (obj: T): Promise<T> => this._model.create({ ...obj });
+
+  read = async (): Promise<T[]> => this._model.find();
+
+  readOne = async (id: string): Promise<T | null> => this._model.findById(id);
+
+  update = async (id: string, obj: Partial<T>) =>
+    this._model.findByIdAndUpdate(id, obj, { new: true });
+
+  delete = async (id: string): Promise<T | null> => this._model.findByIdAndDelete(id);
+}
+
+export default MongoModel;
