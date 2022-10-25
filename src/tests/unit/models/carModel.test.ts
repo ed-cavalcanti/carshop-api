e7e324f580c4292e09b3adf3b6053fdcb19b6e54
ed-cavalcanti <edney2809@gmail.com>
2022-10-25 17:06:49 -0300

    [test] method readOne at CarModel

--- src/tests/unit/models/carModel.test.ts
+++ src/tests/unit/models/carModel.test.ts
@@ -9,16 +9,32 @@ describe('Car Model', () => {
 
   before(() => {
     sinon.stub(Model, 'create').resolves(carMockWithId);
+    sinon.stub(Model, 'findOne').resolves(carMockWithId); 
   });
 
   after(() => {
     sinon.restore();
-  })
+  });
 
   describe('adding a car', () => {
     it('successfully created', async () => {
       const newCar = await carModel.create(carMock);
       expect(newCar).to.be.deep.equal(carMockWithId); 
-    })
-  })
-})
+    });
+  });
+
+  describe('searching a car', () => {
+    it('Successfully found', async () => {
+      const carFound = await carModel.readOne(carMockWithId._id);
+      expect(carFound).to.be.deep.equal(carMockWithId);
+    });
+
+    it('_id not found', async () => {
+      try {
+        await carModel.readOne('wrong_id');
+      } catch (err: any) {
+        expect(err.message).to.be.equal('Id must have 24 hexadecimal characters');
+      }
+    });
+  });
+});
